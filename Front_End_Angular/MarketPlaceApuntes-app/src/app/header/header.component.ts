e300f92caaec686811aea6db594706c1d2f4abda
BrunoIglina <brunitoiglina@gmail.com>
2024-08-08 10:43:26 -0300

    cambios en problemas de sobrescritura

--- Front_End_Angular/MarketPlaceApuntes-app/src/app/header/header.component.ts
+++ Front_End_Angular/MarketPlaceApuntes-app/src/app/header/header.component.ts
@@ -1,18 +1,9 @@
 import { Component } from "@angular/core";
-<<<<<<< HEAD
 import { RouterLink, RouterOutlet } from '@angular/router';
 @Component({
     selector: 'app-header',
     standalone: true,
     imports: [ RouterOutlet, RouterLink],
-=======
-
-
-
-@Component({
-    selector: 'app-header',
-    standalone: true,
->>>>>>> 75c6c6a303f57c16c203a04bcdcf9a78bbbcc6fb
     templateUrl:'./header.component.html',
     styleUrl: './header.component.css'
 
--- Front_End_Angular/MarketPlaceApuntes-app/src/main.ts
+++ Front_End_Angular/MarketPlaceApuntes-app/src/main.ts
@@ -1,5 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-<<<<<<< HEAD
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
@@ -11,16 +10,3 @@ bootstrapApplication(AppComponent, {
 }).catch((err) => console.error(err));
 
 
-
-  
-
- 
-=======
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { HeaderComponent } from './app/header/header.component'; 
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-  bootstrapApplication(HeaderComponent);
->>>>>>> 75c6c6a303f57c16c203a04bcdcf9a78bbbcc6fb
